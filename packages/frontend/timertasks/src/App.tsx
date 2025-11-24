5dccf0f6cb50e5f05fd4a61af96399c8bab42bd7
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2025-11-23 21:47:45 -0300

    start

--- packages/frontend/timertasks/src/App.tsx
+++ packages/frontend/timertasks/src/App.tsx
@@ -1,11 +1,7 @@
 import "./layout/styles/global.css";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-center bg-White">
-      Hello Vite + React
-    </h1>
-  );
+  return <h1>Hello Vite + React</h1>;
 }
 
 export default App;
