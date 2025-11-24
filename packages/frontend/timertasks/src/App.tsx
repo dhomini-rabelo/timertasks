0fff8791e3cbdf91d8fc9aac2b2c91fe21faa5ed
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2025-11-23 21:25:56 -0300

    install tailwind

--- packages/frontend/timertasks/src/App.tsx
+++ packages/frontend/timertasks/src/App.tsx
@@ -1,7 +1,11 @@
 import "./layout/styles/global.css";
 
 function App() {
-  return <h1>Hello Vite + React</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-center bg-amber-900">
+      Hello Vite + React
+    </h1>
+  );
 }
 
 export default App;
