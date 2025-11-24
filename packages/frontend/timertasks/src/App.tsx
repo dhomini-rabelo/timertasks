6cea6b1e2e4933157f5f5527d953f2f96ee7cfb1
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2025-11-23 21:29:52 -0300

    config tailwind

--- packages/frontend/timertasks/src/App.tsx
+++ packages/frontend/timertasks/src/App.tsx
@@ -2,7 +2,7 @@ import "./layout/styles/global.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-center bg-amber-900">
+    <h1 className="text-3xl font-bold text-center bg-White">
       Hello Vite + React
     </h1>
   );
