bdb26fc9cf2d1d2814a7e85d9a6401dfdc6a4844
dhomini-rabelo <86575834+dhomini-rabelo@users.noreply.github.com>
2025-11-28 22:40:23 -0300

    feat: add box

---
+++ packages/frontend/timertasks/src/layout/components/atoms/Box/index.tsx
@@ -0,0 +1,23 @@
+import type { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+interface BoxProps extends HTMLAttributes<HTMLDivElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export function Box({ children, className, ...props }: BoxProps) {
+  return (
+    <div
+      className={twMerge(
+        "bg-White rounded-[24px] shadow-[0px_12px_40px_rgba(0,0,0,0.25)] border border-Black-600",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Box;
